5a5d3c23b5f0927382e6073c91134b44df866085
Elizabeth Baron <elizabeth.baron@teachforamerica.org>
2019-10-09 14:22:59 +0000

    removed AKS project

--- SLN_AdventureWorksExample/aksWindowsPreview/changeSqlLogin.ps1
+++
@@ -1,3 +0,0 @@
-﻿Write-Verbose "Changing SA login credentials"
-$sqlcmd = "ALTER LOGIN sa with password='" + $env:sa_password +"',CHECK_POLICY=OFF,CHECK_EXPIRATION=OFF" + ";ALTER LOGIN sa ENABLE;"
-& sqlcmd -Q $sqlcmd
